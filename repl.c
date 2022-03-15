04e9be93628aaa89ff963c3ac3d6ad3b4e249d41
Garklein <garklein97@gmail.com>
2022-03-15 17:27:58 -0400

    hardcoding the prelude into the binary

--- repl.c
+++ repl.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "prelude.c"
 #include "mlatuMacros.h"
 V pr(T t) { PF("|-> "); prettyTerms(t); PF("\n"); }
-V f(C n,rule root) {
-	I er=parseFile(n,root); if (er) { switch(er) {
-		case OPEN:  PF("Error opening file '%s'\n",n); B;
-		case PRN:   PF("Error parsing file '%s': unbalanced parentheses\n",n); B;
-		case SEMI:  PF("Error parsing file '%s': exactly one semicolon expected in each rule\n",n); B;
-		case EQ:    PF("Error parsing file '%s': exactly one equal sign expected in each rule\n",n); B;
-		case EMPTY: PF("Error parsing file '%s': cannot match with empty LHS\n",n); B;
-		case END:   PF("Error parsing file '%s': semicolon expected at end of every rule\n",n); B;
-		case MCH:   PF("Error parsing file '%s': quotes are opaque and cannot be matched on\n",n); B; } exit(-1); } }
-I main(I ac,C *av) { I dbg=0; C s=ma(100); rule root=newRoot(); f("prelude.mlt",root); term ast;
-	DO(ac-1,if(!strcmp("-d",av[i+1]))dbg=1;else f(av[i+1],root)); // flags and files
+V e(I er,C n) { if (er) { switch(er) {
+	case OPEN:  PF("Error opening file '%s'\n",n); B;
+	case PRN:   PF("Error parsing file '%s': unbalanced parentheses\n",n); B;
+	case SEMI:  PF("Error parsing file '%s': exactly one semicolon expected in each rule\n",n); B;
+	case EQ:    PF("Error parsing file '%s': exactly one equal sign expected in each rule\n",n); B;
+	case EMPTY: PF("Error parsing file '%s': cannot match with empty LHS\n",n); B;
+	case END:   PF("Error parsing file '%s': semicolon expected at end of every rule\n",n); B;
+	case MCH:   PF("Error parsing file '%s': quotes are opaque and cannot be matched on\n",n); B; } exit(-1); } }
+I main(I ac,C *av) { I dbg=0; C s=ma(100), n; rule root=newRoot(); I er=parseRules(prelude,root); e(er,"prelude"); term ast;
+	DO(ac-1,if(!strcmp("-d",n=av[i+1]))dbg=1;else{I er=parseFile(n,root);e(er,n);}); // flags and files
 	while (fgets(s,100,stdin)) { s[strlen(s)-1]='\0'; I er; ast=parseTerms(s,&er);
 		if (er) { switch (er) { 
 			case PRN:   PF("X-> parsing error: unbalanced parentheses\n"); B;
