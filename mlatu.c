7cc7bfa23f6f503b7bf167e8f389de86f7df9eb0
Garklein <garklein97@gmail.com>
2022-04-24 20:33:15 -0400

    fix memory leak

--- mlatu.c
+++ mlatu.c
@@ -24,14 +24,14 @@ V P(C t,I *i,I *er,I lvl,T *c/* n/c */) { I st=*i; do switch (t[*i]) { // parser
 	case ')': WD; if (lvl==0) ER(PRN); R; } while ((*i)++<strlen(t)); if (lvl) ER(PRN); }
 T parseTerms(C s,I *er) { T t=nT(ST,""); I i=0; *er=0; P(s,&i,er,0,&t->n); R t; }
 I parseRule(C s,D d) { I l=strlen(s), cm=0; C nS=ma(l+1); strcpy(nS,s); DO(l,if(cm=cm?s[i]!='\n':s[i]=='#')nS[i]=' ');
-	I e=1; T t=parseTerms(nS,&e); fr(nS); if (e==PRN) R e;
+	I e=1; T iT, t; iT=t=parseTerms(nS,&e); fr(nS); if (e==PRN) R e;
 	I eq=0, per=0; MAP(t,if(*c->w=='=')eq++;if(*c->w=='.')per++;if(!eq&&c->c)R MCH);
 	if (eq!=1) R EQ; if (*t->n->w=='=') R EMPTY; if (per!=1) R PRD; if (*c->w!='.') R END;
 	{MAP(t,if(*c->n->w=='.'){fT(c->n);c->n=0;B;})}
 	t=t->n; D n; while (1) { if (*t->w=='=') { freeTerms(d->r); d->r=t->n; if (!t->n) d->e=1; t->n=0; B; }
 		n=0; MAP(d->c,if(!strcmp(t->w,c->w))n=c);
 		if (!n||!d->c) { n=nD(t->w,0); n->l=d->l+1; if (d->c) c->n=n; else d->c=n; } d=n; t=t->n; }
-	freeTerms(t); R 0; } 
+	freeTerms(iT); R 0; } 
 I parseFile(C n,D root) { FILE *f=fopen(n,"r"); if (!f) R OPEN; I pos, l=0, d, st=ftell(f), r=0, cm=0;
 	while (1) { d=fgetc(f); if (d==-1) B; if (d!=' '&&d!='\n'&&d!='\t'&&d!='\r'&&d!='#'&&!cm) r=1; l++;
 		if (!(cm=cm?d!='\n':d=='#')&&d=='.') { pos=ftell(f); C s=ma(l+1); fseek(f,st,SEEK_SET); fread(s,1,l,f); s[l]=0;
