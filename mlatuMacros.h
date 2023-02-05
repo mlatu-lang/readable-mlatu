9ff5de07dbe694acc4d49cace6c31cff63081217
Garklein <garklein97@gmail.com>
2023-02-05 10:39:44 -0500

    macro twiddling

--- mlatuMacros.h
+++ mlatuMacros.h
@@ -1,12 +1,15 @@
 #ifndef mlatuMacros_h 
 #define mlatuMacros_h
-typedef int I; typedef char *C; typedef void V; 
+typedef int I; typedef char *S; typedef void V;
+#define C case
 #define B break
 #define R return
-#define Z static
+#define _ static
 #define PF printf
 #define fr(x) free(x)
 #define ma(x) malloc(x)
-#define DO(n,x) { I _n=(n); for (I i=0;i<_n;i++) { x; } }
-#define MAP(_d,x) typeof(_d) c=_d, _n; if (c) { while (1) { _n=c->n; x; if (!_n) break; c=_n; } }
+#define sc(d,s) strcpy(d,s)
+#define P(x,...) if (x) R __VA_ARGS__ // panic
+#define DO(n,x) { I _n=(n); for (I i=0;i<_n;i++) { x; } } // no stinkin loops
+#define MAP(_d,x) typeof(_d) c=_d, _n; if (c) { while (1) { _n=c->n; x; if (!_n) break; c=_n; } } // map over linked list
 #endif
--- repl-src/repl.c
+++ repl-src/repl.c
@@ -6,12 +6,12 @@
 #include "../mlatu.h"
 #include "../mlatuMacros.h"
 
-I dbg=0, tmr=0, cnt=0;
+I dbg, tmr, cnt;
 V prD(D d,I i) { DO(i,PF("  ")); PF(" %s: ",d->w);
-	if (d->r) { C s=prettyTerms(d->r); PF("%s",s); fr(s); } else if (d->e) PF("[empty rewrite]");
+	if (d->r) { S s=prettyTerms(d->r); PF("%s",s); fr(s); } else if (d->e) PF("[empty rewrite]");
 	PF("\n"); MAP(d->c,prD(c,i+1)); }
-I fI, fL=100; C *f; V aF(C s) { C nS=malloc(strlen(s)+1); strcpy(nS,s); if (fI==fL) f=realloc(f,fL+=100); f[fI++]=nS; } // file list, add file
-void e(I er,C n,I ex,D root) { if (er) { if (!ex) PF(" "); switch(er) {
+I fI, fL=100; S *f; V aF(S s) { S n=malloc(strlen(s)+1); strcpy(n,s); if (fI==fL) f=realloc(f,fL+=100); f[fI++]=n; } // add file
+void e(I er,S n,I ex,D root) { if (er) { if (!ex) PF(" "); switch(er) {
 	case OPEN:  PF("error opening file '%s'\n",n); B;
 	case PRN:   PF("error parsing file '%s': unbalanced parentheses\n",n); B;
 	case PRD:   PF("error parsing file '%s': exactly one period expected in each rule\n",n); B;
@@ -20,7 +20,7 @@ void e(I er,C n,I ex,D root) { if (er) { if (!ex) PF(" "); switch(er) {
 	case END:   PF("error parsing file '%s': period expected at end of every rule\n",n); B;
 	case MCH:   PF("error parsing file '%s': quotes are opaque and cannot be matched on\n",n); B; }
 	if (ex) { DO(fI,fr(f[i])); freeRules(root), fr(f), exit(-1); } } }
-V sys(C s,D root) { C t=strtok(s," "); C n=strtok(0," ");
+V sys(S s,D root) { S t=strtok(s," "); S n=strtok(0," ");
 	if (!strcmp(t,")h")&&!n) PF(
 		" )h             you are here\n"
 		" bye            exit\n"
@@ -41,22 +41,22 @@ V sys(C s,D root) { C t=strtok(s," "); C n=strtok(0," ");
 V pT(I ms) { I h=ms/3600000, m=(ms-h*3600000)/60000, s=(ms-m*60000-h*3600000)/1000; ms%=1000;
 	switch (h?0:m?1:s?2:3) { case 0: PF("%dh ",h); case 1: PF("%dm ",m); case 2: PF("%ds ",s); case 3: PF("%dms",ms); } }
 
-V P(T ast) { C s=prettyTerms(ast); PF("|-> %s\n",s); fr(s); }
-I main(I ac,C *av) { char s[999]; D root=newRoot(); I er=parseRules(prelude,root); e(er,"prelude",1,root); T ast;
-	f=malloc(fL); C n; DO(ac-1,e(parseFile(n=av[i+1],root),n,1,root);aF(n)); // files
+V O(T ast) { S s=prettyTerms(ast); PF("|-> %s\n",s); fr(s); }
+I main(I ac,S *av) { char s[999]; D root=newRoot(); I er=parseRules(prelude,root); e(er,"prelude",1,root); T ast;
+	f=malloc(fL); S n; DO(ac-1,e(parseFile(n=av[i+1],root),n,1,root);aF(n)); // files
 	PF(" readable-mlatu repl - github.com/mlatu-lang/readable-mlatu\n bye to exit, )h for help\n");
 	Time st, pr, fn; I ms, sc, m, h;
-	while (fgets(s,999,stdin)) { s[strlen(s)-1]=0; if (*s==')'||!strncmp(s,"#wield ",7)) { sys(s,root); continue; } if (tmr) getTime(&st);
-		C i=strchr(s,'#'); if (i) *i=0; ast=parseTerms(s,&er); if (er) { switch (er) { 
+	while (fgets(s,999,stdin)) { s[strlen(s)-1]=0; if (*s==')'||!strncmp(s,"#wield ",7)) { sys(s,root); continue; }
+		if (tmr) getTime(&st); S i=strchr(s,'#'); if (i) *i=0; ast=parseTerms(s,&er); if (er) { switch (er) {
 			case PRN: PF("X-> parsing error: unbalanced parentheses\n"); B;
 			case EQ:  PF("X-> parsing error: equal sign\n\
     if you are trying to define a rule, it cannot be defined in the repl, you need to load it from a file\n"); B;
 			case PRD: PF("X-> parsing error: period\n\
     if you are trying to define a rule, it cannot be defined in the repl, you need to load it from a file\n"); B;
 		} freeTerms(ast); continue; }
-		I show=1, n=0; if (dbg) { while (!stepRewrite(root,ast)) { n++; show=0; P(ast); } } else n=rewrite(root,ast); 
+		I show=1, n=0; if (dbg) { while (!stepRewrite(root,ast)) { n++; show=0; O(ast); } } else n=rewrite(root,ast);
 		I l=0; MAP(ast,l++); if (l==2&&!strcmp("bye",ast->n->w)) { freeTerms(ast); B; };
-		if (show) { getTime(&pr); P(ast); } if (cnt) PF(" took %d rewrite%s\n", n, n==1?"":"s");
+		if (show) { getTime(&pr); O(ast); } if (cnt) PF(" took %d rewrite%s\n", n, n==1?"":"s");
 		if (tmr) { getTime(&fn); ms=milliDiff(&st,&fn); PF(" "); pT(ms);
 			if (!dbg) { ms=milliDiff(&pr,&fn); PF(" ("); pT(ms); PF(" outputting)"); } PF("\n"); }
 		freeTerms(ast); }
