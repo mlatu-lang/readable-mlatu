176a7832e3e28cbf8139132daab523f9bf424690
Garklein <garklein97@gmail.com>
2025-04-28 15:47:41 -0400

    new halt.c

--- examples/halt.c
+++ examples/halt.c
@@ -1,6 +1,6 @@
 // halt.c - find all infinitely looping programs of a certain length
 // usage: ./halt <length>
-// since the halting problem is... the halting problem, this uses the heuristic that if a program can still be rewritten after 1000 rewrites, or if it grows to over 100 terms [see the memory muncher (+,+<)+<], it loops forever.
+// since the halting problem is... the halting problem, this uses the heuristic that if a program can still be rewritten after 1000 rewrites, or if it grows to include over 1000 terms [see the memory munchers (+,+<)+< and (A)(~+,~+<)+<], it loops forever.
 // on 8 and above, will eventually be killed because of memory leaks... i should really fix those
 
 #include <stdio.h>
@@ -9,9 +9,12 @@
 #include "../mlatu.h"
 #include "../mlatuHelpers.c"
 I parses(S s,T *ast) { E e=parseTerms(s,ast); FR(e.f); R !e.e; }
-I big(T ast) { I i=0; MAP(ast,i++); R i>100; }
-I small(T*ast,D r) { DO(1000,{P(stepRewrite(r,ast),1);P(big(*ast),0);}); R 0; }
+I size(T ast) { I i=0; MAP(ast,i++;!c->w&&(i+=size(c->c))); R i; }
+I small(T*ast,D r) { DO(1000,{P(stepRewrite(r,ast),1);P(size(*ast)>1000,0);}); R 0; }
+I comb(T ast) { P(!ast,0); MAP(ast,P(c->w?!strchr("ABCDE",*c->w):!comb(c->c),0)); R 1; }
 I ipow(I b,I p) { I r=1; DO(p,r*=b); R r; }
+I pr(T ast) { S s=prettyTerms(ast); puts(s); FR(s); }
 S cs="()+-<>~,";
-I main(I argc,S argv[]){ I l=atoi(argv[1]); D r=newRoot(); char s[l+1]; s[l]=0; T ast;
-	DO(ipow(8,l), I j=i; DO(l,s[i]=cs[j%8];j/=8); parses(s,&ast)&&!small(&ast,r)&&puts(s); freeTerms(ast)); freeRules(r); }
+I oflen(I l) { char s[l+16]; s[l+15]=0; strcpy(s,"(E)(D)(C)(B)(A)"); T ast; D r=newRoot();
+	DO(ipow(8,l), I j=i; DO(l,s[i+15]=cs[j%8];j/=8); parses(s,&ast)&&small(&ast,r)&&comb(ast)&&PF("%s|",s)&&pr(ast); freeTerms(ast)); freeRules(r); }
+I main(I argc,S argv[]){ DO(1+atoi(argv[1]),oflen(i)); }
