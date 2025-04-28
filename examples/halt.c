0272a72bdb9397ad4d858e94e4a75a1b5795b9c5
Garklein <garklein97@gmail.com>
2025-04-28 11:24:15 -0400

    make brute forcing more robust

--- examples/halt.c
+++ examples/halt.c
@@ -1,6 +1,6 @@
 // halt.c - find all infinitely looping programs of a certain length
 // usage: ./halt <length>
-// since the halting problem is... the halting problem, this uses the heuristic that if a program can still be rewritten after 1000 rewrites, it loops forever.
+// since the halting problem is... the halting problem, this uses the heuristic that if a program can still be rewritten after 1000 rewrites, or if it grows to over 100 terms [see the memory muncher (+,+<)+<], it loops forever.
 // on 8 and above, will eventually be killed because of memory leaks... i should really fix those
 
 #include <stdio.h>
@@ -8,8 +8,10 @@
 #include <stdlib.h>
 #include "../mlatu.h"
 #include "../mlatuHelpers.c"
-D r; I loops(S s) { T ast; E e=parseTerms(s,&ast); P(e.e,0); DO(1000,P(stepRewrite(r,&ast),0)); R 1; }
-I ipow(I b,I p) { I r=1; DO(p, r*=b); R r; }
+I parses(S s,T *ast) { E e=parseTerms(s,ast); FR(e.f); R !e.e; }
+I big(T ast) { I i=0; MAP(ast,i++); R i>100; }
+I small(T*ast,D r) { DO(1000,{P(stepRewrite(r,ast),1);P(big(*ast),0);}); R 0; }
+I ipow(I b,I p) { I r=1; DO(p,r*=b); R r; }
 S cs="()+-<>~,";
-I main(I argc,S argv[]){ I l=atoi(argv[1]); r=newRoot(); char s[l+1]; s[l]=0;
-	DO(ipow(8,l), I j=i; DO(l,s[i]=cs[j/ipow(8,i)%8];); loops(s)&&puts(s)); }
+I main(I argc,S argv[]){ I l=atoi(argv[1]); D r=newRoot(); char s[l+1]; s[l]=0; T ast;
+	DO(ipow(8,l), I j=i; DO(l,s[i]=cs[j%8];j/=8); parses(s,&ast)&&!small(&ast,r)&&puts(s); freeTerms(ast)); freeRules(r); }
